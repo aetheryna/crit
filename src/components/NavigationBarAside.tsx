e9ca6b955d851629f103da8300c8f4aa3d49ced1
AetheryNA <mrnaaw98@gmail.com>
2022-06-19 22:12:02 +0530

    [F003] Navigation bar aside fixes

--- src/components/NavigationBarAside.tsx
+++ src/components/NavigationBarAside.tsx
@@ -5,14 +5,17 @@ const NavigationBarAside = () => {
   return (
     <aside className="navigation-bar-aside">
       <div className="navigation-bar-aside__wrapper">
-        <div className="navigation-bar-aside__profile-picture">
-          <Image
-            src={'https://picsum.photos/100'}
-            width={75}
-            height={75}
-            objectFit={'cover'}
-            alt={'profile picture'}
-          />
+        <div className="navigation-bar-aside__profile-block">
+          <div className="navigation-bar-aside__profile-picture">
+            <Image
+              src={'https://picsum.photos/100'}
+              width={50}
+              height={50}
+              objectFit={'cover'}
+              alt={'profile picture'}
+            />
+          </div>
+          <h3>Aethery</h3>
         </div>
         <NavigationBarAsideItems />
       </div>

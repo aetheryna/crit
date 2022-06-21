bac256ed6618b27474d4089a951448519874f92d
AetheryNA <mrnaaw98@gmail.com>
2022-06-21 08:21:24 +0530

    [F003] Header lobby users

--- src/components/HeaderLobby.tsx
+++ src/components/HeaderLobby.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const HeaderLobby = () => {
   const userList = [
@@ -28,20 +29,22 @@ const HeaderLobby = () => {
     <div className="header-lobby">
       <ul className="header-lobby__users">
         {userList.map((user, index) => (
-          <li className="header-lobby__user" key={index}>
-            <div className="header-lobby__user-picture">
-              <Image
-                src={user.image}
-                width={30}
-                height={30}
-                objectFit={'cover'}
-                alt={'username'}
-              />
-            </div>
-            <div className="header-lobby__user-hover-name">
-              <span>{user.username}</span>
-            </div>
-          </li>
+          <Link href={'/home'} passHref key={index}>
+            <li className="header-lobby__user" key={index}>
+              <div className="header-lobby__user-picture">
+                <Image
+                  src={user.image}
+                  width={30}
+                  height={30}
+                  objectFit={'cover'}
+                  alt={'username'}
+                />
+              </div>
+              <div className="header-lobby__user-hover-name">
+                <span>{user.username}</span>
+              </div>
+            </li>
+          </Link>
         ))}
       </ul>
     </div>

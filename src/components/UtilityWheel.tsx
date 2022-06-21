ddca5e975dc99188e73b58360f54674696da4bb2
AetheryNA <mrnaaw98@gmail.com>
2022-06-21 09:18:51 +0530

    [F003] Utility wheel

---
+++ src/components/UtilityWheel.tsx
@@ -0,0 +1,13 @@
+import { ChatIcon } from '@heroicons/react/outline';
+
+const UtilityWheel = () => {
+  return (
+    <aside className="utility-wheel">
+      <button className="utility-wheel__chat">
+        <ChatIcon />
+      </button>
+    </aside>
+  );
+};
+
+export default UtilityWheel;
